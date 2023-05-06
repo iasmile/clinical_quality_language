123809d2e0724649c97e781a91196e57df8d720f
Jonathan Percival <jonathan.i.percival@gmail.com>
2023-05-06 00:36:23 -0600

    Test files for slow ANC parse

---
+++ Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/anc/BaseTest.java
@@ -0,0 +1,51 @@
+package org.cqframework.cql.cql2elm.anc;
+
+import org.apache.commons.lang3.time.StopWatch;
+import org.cqframework.cql.cql2elm.CqlTranslator;
+import org.cqframework.cql.cql2elm.CqlTranslatorOptions;
+import org.cqframework.cql.cql2elm.TestUtils;
+import org.hl7.elm.r1.*;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static org.cqframework.cql.cql2elm.TestUtils.visitFile;
+import static org.cqframework.cql.cql2elm.matchers.Quick2DataType.quick2DataType;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class BaseTest {
+
+   @Test
+   public void testAncDak() throws IOException {
+      var options = CqlTranslatorOptions.defaultOptions();
+
+      int iterations = 1000;
+      int step = 10;
+      long last = 0;
+      var sw = new StopWatch();
+
+      sw.start();
+      for (int i = 0; i < iterations; i ++) {
+         TestUtils.reset();
+         CqlTranslator translator = TestUtils.createTranslator("anc/ANCDT17.cql", options);
+
+         if (i > 0 & i % step == 0) {
+            var millis = sw.getTime(TimeUnit.MILLISECONDS);
+            var time = millis - last;
+            last = millis;
+            System.out.printf("iteration: %s, step time: %s, step avg: %s%n", i, time, (double)time / step);
+         }
+      }
+
+      sw.stop();
+      var millis = sw.getTime(TimeUnit.MILLISECONDS);
+
+      System.out.printf("iterations: %s, time: %s, avg: %s%n", iterations, millis, (double)millis / iterations);
+
+   }
+}
