aeae6c79119d9bfe409874fb6f3dd26217b05927
Jonathan Percival <jonathan.i.percival@gmail.com>
2023-05-06 15:50:58 -0600

    Add more examples

--- Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/anc/BaseTest.java
+++ Src/java/cql-to-elm/src/test/java/org/cqframework/cql/cql2elm/anc/BaseTest.java
@@ -32,7 +32,7 @@ import static org.hamcrest.Matchers.*;
 public class BaseTest {
 
    @Test
-   public void testAncDak() throws IOException {
+   public void testAncDakErrorListener() throws IOException {
       var cs = CharStreams.fromStream(BaseTest.class.getResourceAsStream("ANCContactDataElementsAmbig.cql"));
       cqlLexer lexer = new cqlLexer(cs);
       CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -41,26 +41,68 @@ public class BaseTest {
       parser.addErrorListener(listener); // add ours
       parser.getInterpreter().setPredictionMode(PredictionMode.LL_EXACT_AMBIG_DETECTION);
       parser.library();
+   }
 
-      // ParseInfo parseInfo = parser.getParseInfo();
-      // ATN atn = parser.getATN();
-      // for (DecisionInfo di : parseInfo.getDecisionInfo()) {
-      //    DecisionState ds = atn.decisionToState.get(di.decision);
-      //    String ruleName = cqlParser.ruleNames[ds.ruleIndex];
 
-      //    // System.out.println(ruleName +" -> " + di.toString());
+   @Test
+   public void testAncDakProfile() throws IOException {
+      var cs = CharStreams.fromStream(BaseTest.class.getResourceAsStream("ANCContactDataElementsExtreme.cql"));
+      cqlLexer lexer = new cqlLexer(cs);
+      CommonTokenStream tokens = new CommonTokenStream(lexer);
+      cqlParser parser = new cqlParser(tokens);
+      var listener = new org.antlr.v4.runtime.DiagnosticErrorListener();
+      parser.addErrorListener(listener); // add ours
+      parser.getInterpreter().setPredictionMode(PredictionMode.LL_EXACT_AMBIG_DETECTION);
+      parser.setProfile(true);
+      parser.library();
 
-      //    if (di.ambiguities.size() > 0) {
-      //       System.out.println(ruleName + " -> " + di.toString());
+      ParseInfo parseInfo = parser.getParseInfo();
+      ATN atn = parser.getATN();
+      for (DecisionInfo di : parseInfo.getDecisionInfo()) {
+         DecisionState ds = atn.decisionToState.get(di.decision);
+         String ruleName = cqlParser.ruleNames[ds.ruleIndex];
 
-      //       System.out.println("ambiguities");
-      //       for (var a : di.ambiguities) {
+         System.out.println(ruleName +" -> " + di.toString());
 
-      //          String text = a.input.getText(Interval.of(a.startIndex, a.stopIndex));
-      //          System.out.printf("alts: %s, decision: %s, range: %s-%s, text: %n%s%n", a.ambigAlts.size(), a.decision,
-      //                a.startIndex, a.stopIndex, text);
-      //       }
-      //    }
-      // }
+         if (di.ambiguities.size() > 0) {
+            System.out.println("ambiguities");
+            for (var a : di.ambiguities) {
+
+               String text = a.input.getText(Interval.of(a.startIndex, a.stopIndex));
+               System.out.printf("alts: %s, decision: %s, range: %s-%s, text: %n%s%n", a.ambigAlts.size(), a.decision,
+                     a.startIndex, a.stopIndex, text);
+            }
+         }
+      }
+   }
+
+
+
+   @Test
+   public void testAncDakStepped() throws IOException {
+
+      int iterations = 100;
+      int step = 10;
+      long time = 0;
+
+      var sw = new StopWatch();
+      sw.start();
+      for (int i = 0; i < iterations; i++) {
+         var cs = CharStreams.fromStream(BaseTest.class.getResourceAsStream("ANCContactDataElementsExtreme.cql"));
+         cqlLexer lexer = new cqlLexer(cs);
+         CommonTokenStream tokens = new CommonTokenStream(lexer);
+         cqlParser parser = new cqlParser(tokens);
+         parser.library();
+
+         if (i > 0 && i % step == 0) {
+            var mills = sw.getTime(TimeUnit.MILLISECONDS);
+            var diff = mills - time;
+            time = mills;
+
+            System.out.printf("iteration: %s, avg ms %s%n", i, (double) diff / step);
+
+         }
+      }
+
    }
 }
